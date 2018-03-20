67d16a695f33f04722b669d45b3a686e4ee27897
Angel Ramírez Martínez <angelooger@gmail.com>
2018-03-20 12:57:54 -0600

    Clase 20 de marzo 12:57

--- PruebaGUI3/src/pruebagui3/Ventana1.java
+++ PruebaGUI3/src/pruebagui3/Ventana1.java
@@ -5,12 +5,21 @@
  */
 package pruebagui3;
 
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
  * @author Angel
  */
 public class Ventana1 extends JFrame{
+    private JLabel etiqueta1;
+    private JLabel etiqueta2;
+    private JLabel etiqueta3;
     
+    public Ventana1(){
+        super("Etiquetas");
+        this.setLayout(new FlowLayout());
+    }
 }
